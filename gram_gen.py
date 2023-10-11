83d2534e1c4af3b616b607a8c8c85c6cf953d83f
Hakase <davidsommerfield.zg@gmail.com>
2023-10-10 21:39:05 -0500

    gram generator

--- gram_gen.py
+++ gram_gen.py
@@ -1,1 +1,50 @@
-TEMPLATES = {"bigram": (2, 0), "1-skipgram": (2, 1)}
+import concurrent.futures
+from collections import Counter
+
+
+def get_grams(corpus, valid=set("abcdefghijklmnopqrstuvwxyz[]\;',./`1234567890-= ")):
+    DATA_TYPES = {
+        "characters": (1, 0),
+        "bigrams": (2, 0),
+        "1-skipgram": (2, 1),
+        "2-skipgram": (2, 2),
+        "3-skipgram": (2, 3),
+        "4-skipgram": (2, 4),
+        "5-skipgram": (2, 5),
+        "6-skipgram": (2, 6),
+        "7-skipgram": (2, 7),
+    }
+
+    unshift = str.maketrans('!@#$%^&*()_+:{}:<>|?"', "1234567890-=;[];,.\/'")
+    text = open(corpus, "r").read().lower().translate(unshift)
+
+    def create(alias, size, skip):
+        grams = Counter()
+
+        for i in range(len(text) - size - skip + 1):
+            window = text[i : i + size + skip]
+
+            # make an n-gram that skips letters between
+            gram = window[: size // 2] + window[-size // 2]
+
+            # only accept valid characters
+            if all(char in valid for char in gram):
+                grams[gram] += 1
+
+        # Output a TSV file of this gram-data subtype
+        with open(f"data/{alias}.txt", "w") as f:
+            for chars, count in sorted(grams.items(), key=lambda x: -x[1]):
+                f.write(f"{chars}\t{count}\n")
+
+    threads = []
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = [
+            executor.submit(create, alias, size, skip)
+            for alias, (size, skip) in DATA_TYPES.items()
+        ]
+        concurrent.futures.wait(futures)
+
+
+if __name__ == "__main__":
+    get_grams("res/iweb-corpus-samples-cleaned.txt")
--- randomcrap/plotspd.py
+++ randomcrap/plotspd.py
@@ -23,10 +23,14 @@ def get_avg(array):
 ltrs = "juyhnm"
 print(len(list(product(ltrs, repeat=2))))
 
-offset = min([get_avg(gram_data[c+c]) for c in ltrs])
+offset = min([get_avg(gram_data[c + c]) for c in ltrs])
 
 key_combos = sum(
-    [[ltrs[i] + ltrs[j] for j in range(i, len(ltrs)) if ltrs[i] != ltrs[j]] for i in range(len(ltrs))], []
+    [
+        [ltrs[i] + ltrs[j] for j in range(i, len(ltrs)) if ltrs[i] != ltrs[j]]
+        for i in range(len(ltrs))
+    ],
+    [],
 )
 
 print(key_combos)
@@ -40,17 +44,17 @@ for bg in key_combos:
     if bg[1] != bg[0]:
         bg_times += gram_data["".join(reversed(bg))]
 
-    dist_to_times[dist] = dist_to_times.get(dist,[])+bg_times
+    dist_to_times[dist] = dist_to_times.get(dist, []) + bg_times
 
 dists = [(d) for d in dist_to_times.keys()]
-times = [get_avg(dist_to_times[d])-offset for d in dist_to_times.keys()] 
+times = [get_avg(dist_to_times[d]) - offset for d in dist_to_times.keys()]
 
 
-#dists = [(pen.get_bigram_penalties("".join(bg))[6]) for bg in key_combos]
-#times = [
+# dists = [(pen.get_bigram_penalties("".join(bg))[6]) for bg in key_combos]
+# times = [
 #    get_avg(gram_data["".join(bg)] + gram_data["".join(reversed(bg))]) - offset
 #    for bg in key_combos
-#]
+# ]
 
 plt.scatter(dists, times, label="Time to Type by Bigram Distance")
 
@@ -59,29 +63,31 @@ plt.xlabel("Distance")
 plt.ylabel("Time to Type (ms)")
 
 # Regress line:
-slope, intercept, r_value, p_value, std_err = stats.linregress([log2(d) for d in dists], times)
+slope, intercept, r_value, p_value, std_err = stats.linregress(
+    [log2(d) for d in dists], times
+)
 #
-#print("slope", slope, "intercept", intercept)
-x = np.arange(1,2.66,0.05)
+# print("slope", slope, "intercept", intercept)
+x = np.arange(1, 2.66, 0.05)
 regression_line = slope * np.log2(x) + intercept
 
-#plt.plot(x, regression_line, color='blue', label='Regression Line')
+# plt.plot(x, regression_line, color='blue', label='Regression Line')
 
-print("stderr",r_value)
+print("stderr", r_value)
 
 
 slope, intercept, r_value, p_value, std_err = stats.linregress(dists, times)
 regression_line = slope * np.array(dists) + intercept
-#plt.plot(dists, regression_line, color='red', label='Regression Line')
+# plt.plot(dists, regression_line, color='red', label='Regression Line')
 
 print("offset", offset)
 
-regression_line = 15.86750971186548 * np.array(dists) + (117.50531805429226-offset)
-plt.plot(dists, regression_line, color='red', label='Regression Line')
+regression_line = 15.86750971186548 * np.array(dists) + (117.50531805429226 - offset)
+plt.plot(dists, regression_line, color="red", label="Regression Line")
 
-print("stderr",r_value)
+print("stderr", r_value)
 
-#for i, bg in enumerate(key_combos):
+# for i, bg in enumerate(key_combos):
 #    plt.annotate(bg, (dists[i], times[i]))
 
 # function to show plot
